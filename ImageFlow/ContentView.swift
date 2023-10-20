4552b6b55492bda2bd122655564e1fe97b663ae9
rahulon12 <rahulon@icloud.com>
2023-10-19 20:59:08 -0400

    Initial Commit

---
+++ ImageFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageFlow
+//
+//  Created by Rahul Narayanan on 10/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageFlow/ImageFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFlowApp.swift
+//  ImageFlow
+//
+//  Created by Rahul Narayanan on 10/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
